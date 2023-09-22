3a845bd23e08f0452966ecf172978fcd1f16bbeb
gnzsnz <8376642+gnzsnz@users.noreply.github.com>
2023-09-22 12:56:36 +0200

    remove user and pass from command line parameters
     Changes to be committed:
    	modified:   image-files/config/ibc/config.ini.tmpl
    	modified:   image-files/scripts/run.sh
    	deleted:    latest/config/ibgateway/jts.ini
    	deleted:    stable/config/ibgateway/jts.ini

---
+++ image-files/scripts/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export DISPLAY=:1
+
+rm -f /tmp/.X1-lock
+Xvfb :1 -ac -screen 0 1024x768x16 &
+
+if [ -n "$VNC_SERVER_PASSWORD" ]; then
+  echo "Starting VNC server"
+  /root/scripts/run_x11_vnc.sh &
+fi
+
+# replace env variables
+envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
+envsubst < "${TWS_INI}.tmpl" > "${TWS_INI}"
+
+
+/root/scripts/fork_ports_delayed.sh &
+
+/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" -g \
+     "--tws-path=${TWS_PATH}" \
+     "--ibc-path=${IBC_PATH}" "--ibc-ini=${IBC_INI}" \
+     "--on2fatimeout=${TWOFA_TIMEOUT_ACTION}"
